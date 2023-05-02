8901eafc740b2a1e0d35534f32c7f44085538910
esraaobeido <esraaabeido123@gmail.com>
2023-05-02 17:33:14 +0000

    lab14

--- server.js
+++ server.js
@@ -1,30 +1,78 @@
 'use strict';
+require("dotenv").config();
 const express = require("express");
-const data = require('./Movie Data/data.json');
 const app = express();
-const port = 3001;
-let result = [];
 const cors = require('cors');
+const axios= require("axios");
 app.use(cors());
+const movieKey = process.env.API_KEY;
+const port = process.env.PORT;
 
 // Create Movie constructor
-function Movie(title, poster_path, overview){
+function Movie(id, title, release_date, poster_path, overview){
+    this.id = id;
     this.title = title;
+    this.release_date = release_date;
     this.poster_path = poster_path;
     this.overview = overview;
-    result.push(this); 
 }
 
-//Create Home Page Endpoint
-app.get('/', (req, res) => {
-    let newMovie = new Movie(data.title, data.poster_path, data.overview);
-    res.json(result);
-});
+// Routes
+app.get("/", handleHome);
+app.get("/favorite", handleFavorite);
+app.get("/trending", handleTrending);
+app.get("/search", handleSearchByName);
+app.get("/videos", handleVideos);
+app.get("/genre", handleGenre);
 
-//Create Favorite Page Endpoint
-app.get('/favorite', (req, res) => {
-    res.send('Welcome to Favorite Page');
-});
+//handlers
+function handleHome(req, res){
+  let movies = data.map((item) => {
+    return new Movie(item.id, item.title, item.release_date, item.poster_path, item.overview);
+  })
+  res.json(movies);
+}
+
+function handleFavorite(req, res){
+  res.send('Welcome to Favorite Page');
+}
+
+async function handleTrending(req, res){
+  const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${movieKey}`;
+  let movieFromAPI = await axios.get(url);
+  let movies = movieFromAPI.data.results.map((item) => {
+    return new Movie(item.id, item.title, item.release_date, item.poster_path, item.overview);
+  })
+    res.send(movies)
+  }
+
+function handleSearchByName(req, res){
+  let searchByName = req.query.name;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=${searchByName}`
+  axios.get(url).then((result)=>{
+   console.log(result.data);
+   res.send(result.data.results)
+  })
+  .catch((error) =>{
+   res.status(500).send(error, "error");
+  })
+ }
+
+const MOVIE_ID = 123;
+async function handleVideos(req, res) {
+  const movieId = req.query.id;
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${movieKey}`;
+  let movieFromAPI = await axios.get(url);
+  const videos = movieFromAPI.data.results;
+  res.send(videos);
+}
+
+async function handleGenre(req, res) {
+  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${movieKey}`
+  let movieFromAPI = await axios.get(url);
+  const genres = movieFromAPI.data.genres;
+  res.send(genres);
+}
 
 //function to handle 404 Errors
 app.use((req, res, next) => {
